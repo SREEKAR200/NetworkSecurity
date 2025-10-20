ba060c98a5e974081e7c7fced8d24e0a63975798
Sreekar <sreekarkannepalli4@gmail.com>
2025-10-20 11:40:31 +0530

    The password is masked

--- test_mongodb.py
+++ test_mongodb.py
@@ -1,7 +1,7 @@
 
 from pymongo.mongo_client import MongoClient
 
-uri = "mongodb+srv://sreekarkannepalli4:Sreekar1@cluster0.zznhbm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+uri = "mongodb+srv://sreekarkannepalli4:<@password>@cluster0.zznhbm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 # Create a new client and connect to the server
 client = MongoClient(uri)
