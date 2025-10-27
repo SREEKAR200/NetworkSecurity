ec3b2d0ab2189fdde7aa337d992fc169d34defc6
Sreekar <sreekarkannepalli4@gmail.com>
2025-10-27 21:06:19 +0530

    Data Validation

--- NetworkSecurity/components/data_validation.py
+++ NetworkSecurity/components/data_validation.py
@@ -26,7 +26,7 @@ class DataValidation:
             raise NetworkSecurityException(e,sys)
     def validate_number_of_columns(self,dataframe:pd.DataFrame)-> bool:
         try:
-            number_of_Columns=len(self.._schema_config)
+            number_of_Columns=len(self.schema_config)
             logging.info(f"Required Number of Columns:{number_of_Columns}")
             logging.info(f"DataFrame has Columns:{len(dataframe.columns)}")
         except Exception as e:
@@ -37,7 +37,7 @@ class DataValidation:
             status=True
             report={}
             for column in base_df.columns:
-                d1=base_df{column}
+                d1=base_df[column]
                 d2=current_df[column]
                 is_same_dist=ks_2samp(d1,d2)
                 if threshold<=is_same_dist.pvalue:
@@ -65,12 +65,30 @@ class DataValidation:
 
             train_dataframe=DataValidation.read_data(train_file_path)
             test_dataframe=DataValidation.read_data(test_file_path)
+            error_message=" "
             status=self.validate_number_of_columns(dataframe=train_dataframe)
             if not status:
                 error_message=f"{error_message} Train dataframe does not contain all columns"
             status=self.validate_number_of_columns(dataframe=test_dataframe)
             if not status:
                 error_message=f"{error_message} Test dataframe does not contain all columns"
+            status=self.detect_dataset_drift(base_df=train_dataframe,current_df=test_dataframe)
+            dir_path=os.path.dirname(self.data_validation_config.valid_train_file_path)
+            os.makedirs(dir_path,exist_ok=True)
+            train_dataframe.to_csv(
+                self.data_validation_config.valid_train_file_path,index=False, header=True
+            )
+            test_dataframe.to_csv(
+                self.data_validation_config.valid_test_file_path,index=False, header=True
+            )
+            data_validation_artifact=DataValidationArtifact(
+                validation_status=status,
+                valid_train_file_path=self.data_ingestion_artifact.trained_file_path,
+                valid_test_file_path=self.data_ingestion_artifact.test_file_path,
+                invalid_train_file_path=None,
+                invalid_test_file_path=None,drift_report_file_path=self.data_validation_config.drift_report_file_path,
+            )
+            return data_validation_artifact
         except Exception as e:
-            raise NetworkSecurityException
+            raise NetworkSecurityException(e,sys) from e
 
--- NetworkSecurity/utils/main_utils/utils.py
+++ NetworkSecurity/utils/main_utils/utils.py
@@ -18,8 +18,8 @@ def write_yaml_file(file_path:str,content:object,replace:bool=False)->None:
         if replace:
             if os.path.exists(file_path):
                 os.remove(file_path)
-        os.makedirs(os.path.dirname{file_path},exist_ok=True)
-        with open(file_path="w")as file:
+        os.makedirs(os.path.dirname(file_path),exist_ok=True)
+        with open(file_path,"w")as file:
             yaml.dump(content,file)
     except Exception as e:
         raise NetworkSecurityException(e,sys)
